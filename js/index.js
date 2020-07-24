da3106fa14e9bf4103921fbd504fcefb8839fd74
rainy-me <github@rainy.me>
2020-07-24 23:30:40 +0900

    we don't need map here

--- js/index.js
+++ js/index.js
@@ -2,6 +2,26 @@ const fs = require("fs");
 const path = require("path");
 const vm = require("vm");
 
+function main() {
+  const asScript = [];
+  const noError = [];
+  const [_, ...emojis] = fs
+    .readFileSync(path.join(__dirname, "../emoji.txt"), { encoding: "utf8" })
+    .split("\n");
+  for (const emoji of emojis) {
+    let script;
+    ok(() => (script = new vm.Script(emoji))) &&
+      asScript.push(emoji) &&
+      ok(() => script.runInNewContext()) &&
+      noError.push(emoji);
+  }
+  console.log(`valid as script:`);
+  console.log(asScript);
+
+  console.log(`runs without error:`);
+  console.log(noError);
+}
+
 const ok = (fn) => {
   try {
     fn();
@@ -10,25 +30,5 @@ const ok = (fn) => {
     return false;
   }
 };
-
-function main() {
-  const asScript = [];
-  const noError = [];
-  const [_, ...emojis] = fs
-    .readFileSync(path.join(__dirname, "../emoji.txt"), { encoding: "utf8" })
-    .split("\n");
-  emojis.map((emoji) => {
-    let script;
-    ok(() => (script = new vm.Script(emoji))) &&
-      asScript.push(emoji) &&
-      ok(() => script.runInNewContext()) &&
-      noError.push(emoji);
-  });
-  console.log(`valid as script:`);
-  console.log(asScript);
-
-  console.log(`runs without error:`);
-  console.log(noError);
-}
 
 main();
